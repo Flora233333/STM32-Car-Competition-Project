aa2031b05b3a51d718f228cfd8c36e339c50d3c7
Flora <1005214199@qq.com>
2022-08-31 17:06:33 +0800

    配置编码器模式

--- Hardware/PWM.c
+++ Hardware/PWM.c
@@ -1,41 +1,72 @@
 #include "PWM.h"              
 
-void PWM_Init(void) {
-	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE);
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB | RCC_APB2Periph_AFIO, ENABLE);
-	GPIO_PinRemapConfig(GPIO_PartialRemap_TIM3, ENABLE);//重映射
-    
+/**
+  * @brief  初始化TIM2_CH3,TIM2_CH4作为PWM输出
+  * @param  无
+  * @retval 无
+  */
+void PWM_Init() {
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA, ENABLE);
+	
 	GPIO_InitTypeDef GPIO_InitStruct;
 	GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF_PP;
-	GPIO_InitStruct.GPIO_Pin =  GPIO_Pin_5;
+	GPIO_InitStruct.GPIO_Pin =  GPIO_Pin_2 | GPIO_Pin_3;
 	GPIO_InitStruct.GPIO_Speed = GPIO_Speed_50MHz;
-	GPIO_Init(GPIOB, &GPIO_InitStruct);
+	GPIO_Init(GPIOA, &GPIO_InitStruct);
 	
-	TIM_InternalClockConfig(TIM3);
+	TIM_InternalClockConfig(TIM2);
 	
 	TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStruct;
 	TIM_TimeBaseInitStruct.TIM_ClockDivision = TIM_CKD_DIV1;
 	TIM_TimeBaseInitStruct.TIM_CounterMode = TIM_CounterMode_Up;
-	TIM_TimeBaseInitStruct.TIM_Period = 100 - 1; //配灯的PWM频率就不能太快
-	TIM_TimeBaseInitStruct.TIM_Prescaler = 720 - 1;
+	TIM_TimeBaseInitStruct.TIM_Period = 1000 - 1; //PWM频率一定要快，否则电机转起来一顿一顿
+	TIM_TimeBaseInitStruct.TIM_Prescaler = 0;
 	TIM_TimeBaseInitStruct.TIM_RepetitionCounter = 0;
-	TIM_TimeBaseInit(TIM3, &TIM_TimeBaseInitStruct);
+	TIM_TimeBaseInit(TIM2, &TIM_TimeBaseInitStruct);
 	
 	TIM_OCInitTypeDef TIM_OCInitStructure;
 	TIM_OCStructInit(&TIM_OCInitStructure);
 	TIM_OCInitStructure.TIM_OCMode  = TIM_OCMode_PWM1;
 	TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High; 	  
 	TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable; 
-	TIM_OCInitStructure.TIM_Pulse = 100;
+	TIM_OCInitStructure.TIM_Pulse = 0;
     
-	TIM_OC2Init(TIM3, &TIM_OCInitStructure);
-	
-	TIM_Cmd(TIM3, ENABLE);
+	TIM_OC3Init(TIM2, &TIM_OCInitStructure);
+	TIM_OC4Init(TIM2, &TIM_OCInitStructure);
+
+	TIM_Cmd(TIM2, ENABLE);
 }
 
-void PWM_Updata(float angle) {
-    uint16_t out = 0;
-    out = (int)(angle / 90) * 50 + 50;
-    TIM_SetCompare2(TIM3, out);
-    printf("out = %d", out);
+void PWM_Restrict(int *Motor_1, int *Motor_2) {
+	if(*Motor_1 > 800) *Motor_1 = 800;
+	if(*Motor_2 > 800) *Motor_2 = 800;
+	
+	if(*Motor_1 < -800) *Motor_1 = -800;
+	if(*Motor_2 < -800) *Motor_2 = -800;
+}
+
+void PWM_Updata(int Motor_1, int Motor_2) {
+	if(Motor_1 > 0)
+	{
+		Motor1_SetDirct(1, 0);
+	}
+	else 
+	{
+		Motor1_SetDirct(0, 1);
+		Motor_1 = -1 * Motor_1;
+	}
+	
+	if(Motor_2 > 0)
+	{
+		Motor2_SetDirct(1, 0);
+	}
+	else
+	{
+		Motor2_SetDirct(0, 1);
+		Motor_2 = -1 * Motor_2;
+	}
+	
+	TIM_SetCompare3(TIM2, Motor_1);
+	TIM_SetCompare4(TIM2, Motor_2);
 }
--- Hardware/PWM.h
+++ Hardware/PWM.h
@@ -2,9 +2,10 @@
 #define __PWM_H
 
 #include "stm32f10x.h"
-#include "bsp_usart.h"
+#include "Motor.h"
 
 void PWM_Init(void);
-void PWM_Updata(float angle);
+void PWM_Restrict(int *Motor_1, int *Motor_2);
+void PWM_Updata(int Motor_1, int Motor_2);
 
 #endif
